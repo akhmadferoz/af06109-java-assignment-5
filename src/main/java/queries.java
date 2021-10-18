882faa8bf43226dff526e418596efc3b62dbef40
Ahmad Feroz <88190058+akhmadferoz-maqsad@users.noreply.github.com>
2021-10-18 23:27:27 +0500

    [FEAT] DB connection established

--- src/main/java/queries.java
+++ src/main/java/queries.java
@@ -1,3 +1,6 @@
 public class queries {
-
+    static String createTable = "CREATE TABLE IF NOT EXISTS countryLanguageUsage( `Country` varchar(255), `Official Usage (%)` varchar(255), `Non-official Usage (%)` varchar(255))";
+    static String insertEntries = "INSERT INTO countryLanguageUsage, (Country,`Non-official Usage`,`Official Usage`) VALUES ('%s','%s','%s')";
+    static String listCountries = "select * from country";
+    static String listCountryLanguages = "select * from countrylanguage";
 }
