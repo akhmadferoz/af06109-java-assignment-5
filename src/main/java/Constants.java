d86f5d9271bc87f2053cf171523d22ae7b791fd4
Ahmad Feroz <88190058+akhmadferoz-maqsad@users.noreply.github.com>
2021-10-27 02:07:33 +0500

    [FEAT] Forgot to push

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,5 +1,3 @@
 public class Constants {
-    static String host = "mysql://127.0.0.1/";
-    static String user = "guest";
-    static String password = "wtEU422gigdXFF";
+   public static String host = "mysql://127.0.0.1/";
 }
